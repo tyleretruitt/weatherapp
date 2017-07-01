0813bbb56b803ea077dd9b7dc2650661265f2455
Tyler Truitt <tylertruitt12@outlook.com>
2017-07-01 18:36:10 -0500

    Fixed Again For Real This Time Though

--- src/routes.php
+++ src/routes.php
@@ -17,11 +17,7 @@ $app->get('/forecast/[{zipCode}]', function ($request, $response, $args) {
     
     $data = file_get_contents("http://api.openweathermap.org/data/2.5/weather?zip=$zipCode&APPID=9bc48743268143ac3107d78b31282339&units=imperial");
     
-    $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://weatherapp.trucoding.com')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->getBody()->write("$data");
+    $response->getBody()->write("$data");
 
     return $response;
 })
